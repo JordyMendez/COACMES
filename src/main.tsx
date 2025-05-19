03f05124dc3e2abc068211cefbfe9f7ee2e6d743
Jordy Chichanda <jordimendez1999gmail.com>
2025-05-19 15:01:16 -0500

    lOGIN AÑADIDO Y FUNCIONANDO, ASI MISMO EL HOME

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
 import { router } from './routes/routes'
-
+import './index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
